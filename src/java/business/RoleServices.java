3d82c22c0958fda9cc3d266bbdd32e6239690286
843876 <843876@SAIT225726IT.ACDM.DS.SAIT.CA>
2021-03-09 15:00:59 -0700

    fixes, userServices, created viewAll, add, delete, and update methods

--- src/java/business/RoleServices.java
+++ src/java/business/RoleServices.java
@@ -15,28 +15,5 @@ import models.User;
  */
 public class RoleServices {
     
-       public List<User> getAll(String email) throws Exception{
-           UserDB userdb = new UserDB();
-           List<User> user = userdb.getAll(email);
-           return user;
-       }
-       
-       public void insert(String email, boolean active, String firstName, String lastName, String password, int role) throws Exception{
-           User user = new User(email, active, firstName, lastName, password, role);
-           UserDB userDB = new UserDB();
-           userDB.insert(user);
-       }
-       
-       public void delete (String email)throws Exception{
-           User user = new User();
-           user.setEmail(email);
-           UserDB userDB = new UserDB();
-           userDB.delete(user);
-       }
-       
-       public void update(String email, boolean active, String firstName, String lastName, String password, int role) throws Exception{
-           User user = new User(email, active, firstName, lastName, password, role);
-           UserDB userDB = new UserDB();
-           userDB.update(user);
-       }
+
 }
--- src/java/business/UserServices.java
+++ src/java/business/UserServices.java
@@ -10,7 +10,9 @@ import java.util.ArrayList;
 import javax.naming.spi.DirStateFactory.Result;
 import models.User;
 import dataaccess.ConnectionPool;
+import dataaccess.UserDB;
 import java.sql.Connection;
+import java.util.List;
 
 /**
  *
@@ -18,9 +20,30 @@ import java.sql.Connection;
  */
 public class UserServices {
     
-    public ArrayList ViewAllUsers(){
-
-    }
+       public List<User> getAll(String email) throws Exception{
+           UserDB userdb = new UserDB();
+           List<User> user = userdb.getAll(email);
+           return user;
+       }
+       
+       public void insert(String email, boolean active, String firstName, String lastName, String password, int role) throws Exception{
+           User user = new User(email, active, firstName, lastName, password, role);
+           UserDB userDB = new UserDB();
+           userDB.insert(user);
+       }
+       
+       public void delete (String email)throws Exception{
+           User user = new User();
+           user.setEmail(email);
+           UserDB userDB = new UserDB();
+           userDB.delete(user);
+       }
+       
+       public void update(String email, boolean active, String firstName, String lastName, String password, int role) throws Exception{
+           User user = new User(email, active, firstName, lastName, password, role);
+           UserDB userDB = new UserDB();
+           userDB.update(user);
+       }
     
     
 }
