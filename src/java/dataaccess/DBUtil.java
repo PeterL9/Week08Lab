21e9b65d6cf56f1efe8263e54342b4fa0dfecae6
821214 <821214@SAIT225178IT.ACDM.DS.SAIT.CA>
2021-03-09 14:51:58 -0700

    Pulled and merged to commit the completed databaseaccess UserDB, ConnectionPool, and DBUtil are complete

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -1,132 +1,25 @@
 package dataaccess;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-import models.User;
+import java.sql.*;
 
-public class UserDB {
-
-    public List<User> getAll(String owner) throws Exception {
-        List<User> users = new ArrayList<>();
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        
-        String sql = "SELECT * FROM note WHERE owner=?";
-        
+public class DBUtil {
+    public static void closePreparedStatement(Statement ps) {
         try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, owner);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                String email = rs.getString(1);
-                boolean active = rs.getBoolean(2);
-                String firstName = rs.getString(3);
-                String lastName = rs.getString(4);
-                String password = rs.getString(5);
-                String role = rs.getString(6);
-                User user = new User(email, active, firstName, lastName, password, role);
-                users.add(user);
+            if (ps != null) {
+                ps.close();
             }
-        } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+        } catch (SQLException e) {
+            System.out.println(e);
         }
-
-        return users;
     }
 
-    public User get(int noteId) throws Exception {
-        User user = null;
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        String sql = "SELECT * FROM user";
-        
+    public static void closeResultSet(ResultSet rs) {
         try {
-            ps = con.prepareStatement(sql);
-            ps.setInt(1, noteId);
-            rs = ps.executeQuery();
-            if (rs.next()) {
-                String email = rs.getString(1);
-                String active = rs.getString(2);
-                String firstName = rs.getString(3);
-                String lastName = rs.getString(4);
-                String password = rs.getString(5);
-                String role = rs.getString(6);
-                user = new User(email, active, firstName, lastName, password, role);
+            if (rs != null) {
+                rs.close();
             }
-        } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
-        }
-        
-        return user;
-    }
-
-    public void insert(User user) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "INSERT INTO user (email, active, firstName, lastName, password, role) VALUES (?, ?, ?, ?, ?, ?)";
-        
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, user.getEmail());
-            ps.setString(2, user.getActive());
-            ps.setString(3, user.getFirstName());
-            ps.setString(4, user.getLastName());
-            ps.setString(5, user.getPassword());
-            ps.setString(6, user.getRole());
-            ps.executeUpdate();
-        } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+        } catch (SQLException e) {
+            System.out.println(e);
         }
     }
-
-    public void update(User user) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "UPDATE note SET title=?, contents=? WHERE note_id=?";
-        
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, user.getEmail());
-            ps.setString(2, user.getActive());
-            ps.setString(3, user.getFirstName());
-            ps.setString(4, user.getLastName());
-            ps.setString(5, user.getPassword());
-            ps.setString(6, user.getRole());
-            ps.executeUpdate();
-        } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
-        }
-    }
-
-    public void delete(User user) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "DELETE FROM note WHERE note_id=?";
-        
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setInt(1, note.getNoteId());
-            ps.executeUpdate();
-        } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
-        }
-    }
-
 }
