666fae0a649dcb2a387c11b07f9f08e0e68af6b2
799768 <799768@SAIT225783IT.ACDM.DS.SAIT.CA>
2021-03-09 13:44:55 -0700

    Added Code to User model

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -5,10 +5,78 @@
  */
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 799768
  */
-public class User {
+public class User implements Serializable {
+    private String email;
+    private boolean active;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String role;
     
+    public User() {
+        
+    }
+
+    public User(String email, boolean active, String firstName, String lastName, String password, String role) {
+        this.email = email;
+        this.active = active;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
